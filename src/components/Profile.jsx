99e6350f81e8186d8c1c56e2c8ca235e0c605a5c
Piotr Góralski <piotr001goralski@gmail.com>
2025-08-21 15:03:41 +0200

    Revert "Split profile page to 2 separate components"
    This reverts commit ca04fc5cad51fcba9ca923fb2ccf02147cc956d3.

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import {useAuthService} from "../services/AuthService.js";
+import SignInPage from "./SignInPage.jsx";
+import SignUpPage from "./SignUpPage.jsx";
 import LoggedInUserPage from "./LoggedInUserPage.jsx";
 
 export default function Profile() {
@@ -7,11 +9,22 @@ export default function Profile() {
     const { getSignInUser } = useAuthService();
     const user = getSignInUser();
 
-    if (!user.name) {
-        location.replace("/authorize");
-    }
-
     return (
-        user.name ? <LoggedInUserPage /> : <></>
+        <>
+            {
+                !user.name ? (
+                    <main className='profilePage'>
+                        <section id='signUpPage' className="panel dark">
+                            <SignUpPage />
+                        </section>
+                        <section id='signInPage' className="panel light">
+                            <SignInPage />
+                        </section>
+                    </main>
+                ) : (
+                    <LoggedInUserPage />
+                )
+            }
+        </>
     );
 }
--- src/components/SignInPage.jsx
+++ src/components/SignInPage.jsx
@@ -78,7 +78,7 @@ export default function SignInPage() {
                 .then(() => {
                     setSignInError(false);
                     setTimeout(() => {
-                        location.replace("/profile");
+                        location.reload();
                     }, 1500);
                 })
                 .catch(() =>  setSignInError(true));
